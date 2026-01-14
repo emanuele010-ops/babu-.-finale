60d6c74c1979b6b9202ae4ae150aa53528baa98c
Emanuele Masiero <masieroemanuele02@gmail.com>
2026-01-14 11:16:03 +0100

    inizio

---
+++ VIDEOGAME.py
@@ -0,0 +1,117 @@
+"""
+Starting Template
+
+Once you have learned how to use classes, you can begin your program with this
+template.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.starting_template
+"""
+import arcade
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "RUNNING CIKEN"
+
+
+class GameView(arcade.Window):
+    """
+    Main application class.
+
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        self.sprite = None
+        self.playerSpriteList = arcade.SpriteList()
+    
+        self.setup()
+
+    def setup(self):
+        
+        self.sprite = arcade.Sprite("./pollo.png")
+
+        self.sprite.center_x = 100
+        self.sprite.center_y = 100
+        self.sprite.scale_x = 0.2
+        self.sprite.scale_y = 0.2
+
+        self.background_color = arcade.color.BUD_GREEN
+
+        self.playerSpriteList.append(self.sprite)
+
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+
+    def reset(self):
+        """Reset the game to the initial state."""
+        # Do changes needed to restart the game here if you want to support that
+        pass
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        self.clear()
+        self.playerSpriteList.draw()
+
+        # Call draw() on all your sprite lists below
+
+    def on_update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+        pass
+
+    def on_key_press(self, key, key_modifiers, modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+
+        For a full list of keys, see:
+        https://api.arcade.academy/en/latest/arcade.key.html
+        """
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        """
+        Called whenever the user lets off a previously pressed key.
+        """
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
+
+
+def main():
+    game = GameView(
+        600, 600, "Il mio giochino"
+    )
+    arcade.run()
+
+
+
+
+if __name__ == "__main__":
+    main()
